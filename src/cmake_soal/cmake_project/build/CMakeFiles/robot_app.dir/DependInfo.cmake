9a8c9d7514a578b68b496c9c637998874dfb091f
Satria Lhasa <strdwanda@gmail.com>
2026-03-25 23:00:18 +0700

    feat: mengupload folder jawaban cmake

---
+++ src/cmake_soal/cmake_project/build/CMakeFiles/robot_app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/satria/cmake_project/src/main.cpp" "CMakeFiles/robot_app.dir/src/main.cpp.o" "gcc" "CMakeFiles/robot_app.dir/src/main.cpp.o.d"
+  "/home/satria/cmake_project/src/robot.cpp" "CMakeFiles/robot_app.dir/src/robot.cpp.o" "gcc" "CMakeFiles/robot_app.dir/src/robot.cpp.o.d"
+  "/home/satria/cmake_project/src/robotair.cpp" "CMakeFiles/robot_app.dir/src/robotair.cpp.o" "gcc" "CMakeFiles/robot_app.dir/src/robotair.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake_soal/cmake_project/build/CMakeFiles/robot_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/robot_app"
+  "../bin/robot_app.pdb"
+  "CMakeFiles/robot_app.dir/src/main.cpp.o"
+  "CMakeFiles/robot_app.dir/src/main.cpp.o.d"
+  "CMakeFiles/robot_app.dir/src/robot.cpp.o"
+  "CMakeFiles/robot_app.dir/src/robot.cpp.o.d"
+  "CMakeFiles/robot_app.dir/src/robotair.cpp.o"
+  "CMakeFiles/robot_app.dir/src/robotair.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
